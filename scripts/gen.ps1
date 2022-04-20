159f6c19d76686f3d707fcf10f1c72926bcdf28c
Clement Jean <clement.jean@epitech.eu>
2022-04-20 10:46:48 +0800

    windows compat

---
+++ scripts/gen.ps1
@@ -0,0 +1,12 @@
+foreach ( $project in $args ) {
+    grpc_tools_node_protoc -I $project\proto                                       `
+    --js_out=import_style=commonjs:$project\proto                                  `
+    --grpc_out=grpc_js:$project\proto                                              `
+    $project\proto\$project.proto;
+    Get-ChildItem -Filter *.proto -Recurse $project\proto -Exclude $project.proto | ForEach-Object { `
+        $file = ".\$project\proto\" + $_.Name;                                  `
+        grpc_tools_node_protoc -I $project\proto                                    `
+        --js_out=import_style=commonjs:$project\proto                               `
+        $file `
+    }
+}
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,12 +7,12 @@ argc=$#
 argv=("$@")
 
 for (( j = 0; j < argc; ++j )); do
-  grpc_tools_node_protoc -I=${argv[j]}/proto/                                   \
+  grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     --grpc_out=grpc_js:${argv[j]}/proto/                                        \
     --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin  \
     ${argv[j]}/proto/${argv[j]}.proto;
-  grpc_tools_node_protoc -I=${argv[j]}/proto/                                   \
+  grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     $(find ${argv[j]}/proto/ -type f -name "*.proto" -not -path "${argv[j]}/proto/${argv[j]}.proto")
 done
