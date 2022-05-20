e5c9d86848602b16b9b0c07b7ef8a553dee2e9be
Clement <clement.jean@epitech.eu>
2022-05-20 13:33:35 +0800

    set full path to grpc_tools_node_protoc in scripts

--- scripts/gen.ps1
+++ scripts/gen.ps1
@@ -1,11 +1,11 @@
 foreach ( $project in $args ) {
-    grpc_tools_node_protoc -I $project\proto                                       `
+    ./node_modules/.bin/grpc_tools_node_protoc -I $project\proto                                       `
     --js_out=import_style=commonjs:$project\proto                                  `
     --grpc_out=grpc_js:$project\proto                                              `
     $project\proto\$project.proto;
     Get-ChildItem -Filter *.proto -Recurse $project\proto -Exclude $project.proto | ForEach-Object { `
         $file = ".\$project\proto\" + $_.Name;                                  `
-        grpc_tools_node_protoc -I $project\proto                                    `
+        ./node_modules/.bin/grpc_tools_node_protoc -I $project\proto                                    `
         --js_out=import_style=commonjs:$project\proto                               `
         $file `
     }
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,11 +7,11 @@ argc=$#
 argv=("$@")
 
 for (( j = 0; j < argc; ++j )); do
-  grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
+  ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     --grpc_out=grpc_js:${argv[j]}/proto/                                        \
     ${argv[j]}/proto/${argv[j]}.proto;
-  grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
+  ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     $(find ${argv[j]}/proto/ -type f -name "*.proto" -not -path "${argv[j]}/proto/${argv[j]}.proto")
 done
