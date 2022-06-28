8da9c511b640719f2b7ca29a7a06f4b3dd8ccfaa
Clement <clement.jean@epitech.eu>
2022-06-29 07:38:56 +0800

    Add comments to explain what the commands are doing

--- scripts/gen.ps1
+++ scripts/gen.ps1
@@ -1,8 +1,13 @@
 foreach ( $project in $args ) {
+    # Generate gRPC and Protobuf code for ${PROJECT}/${PROJECT}.proto
+    # (eg: greet/greet.proto)
     ./node_modules/.bin/grpc_tools_node_protoc -I $project\proto                                       `
     --js_out=import_style=commonjs:$project\proto                                  `
     --grpc_out=grpc_js:$project\proto                                              `
     $project\proto\$project.proto;
+    
+    # Generate only Protobuf code for all the other .proto files (if any)
+    # (eg: calculator/sum.proto)
     Get-ChildItem -Filter *.proto -Recurse $project\proto -Exclude $project.proto | ForEach-Object { `
         $file = ".\$project\proto\" + $_.Name;                                  `
         ./node_modules/.bin/grpc_tools_node_protoc -I $project\proto                                    `
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -8,10 +8,15 @@ argc=$#
 argv=("$@")
 
 for (( j = 0; j < argc; ++j )); do
+  # Generate gRPC and Protobuf code for ${PROJECT}/${PROJECT}.proto
+  # (eg: greet/greet.proto)
   ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     --grpc_out=grpc_js:${argv[j]}/proto/                                        \
     ${argv[j]}/proto/${argv[j]}.proto;
+  
+  # Generate only Protobuf code for all the other .proto files (if any)
+  # (eg: calculator/sum.proto)
   ./node_modules/.bin/grpc_tools_node_protoc -I ${argv[j]}/proto/                                   \
     --js_out=import_style=commonjs:${argv[j]}/proto/                            \
     $(find ${argv[j]}/proto/ -type f -name "*.proto" -not -path "${argv[j]}/proto/${argv[j]}.proto")
